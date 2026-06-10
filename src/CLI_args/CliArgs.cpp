28bc949ed805c85151758586303492d4b6f228f9
DavidZelaya12 <davidzelaya06.isj@gmail.com>
2026-06-10 17:22:27 -0600

    First CLIargs implementation

---
+++ src/CLI_args/CliArgs.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <format>
+#include "CliArgs.hpp"
+
+void CliArgs::printUsage()
+{
+    std::cout
+        << "Usage: " << program << " --file <file_path>\n"
+        << "\nOptions:\n"
+        << "  --file <file_path>   Specify the file path to process\n";
+}
+
+std::optional<std::string> CliArgs::parse()
+{
+
+    std::cout << "Args count: " << argc_ << "\n";
+
+    for (int i = 0; i < argc_; i++)
+    {
+        if (std::strcmp(argv_[i], "--program") == 0)
+        {
+            if (i + 1 == argc_)
+            {
+                return std::string("Option '--program' requires a file path");
+            }
+            machine_code = argv_[++i];
+        }
+        else if (std::strcmp(argv_[i], "--font") == 0)
+        {
+            if (i + 1 == argc_)
+            {
+                return std::string("Option '--font' requires a file path");
+            }
+            fontpath_ = argv_[++i];
+        }
+        else if (std::strcmp(argv_[i], "--data") == 0)
+        {
+            if (i + 1 == argc_)
+            {
+                return std::string("Option '--data' requires a file path");
+            }
+            datafilepath_ = argv_[++i];
+        }
+        else
+        {
+            return std::format("Invalid option '{}'", argv_[i]);
+        }
+        std::cout << "Parsed argument: " << argv_[i] << "\n";
+    }
+
+    if (machine_code.empty())
+    {
+        return std::string("Missing required option '--program <file_path>'");
+    }
+    if (fontpath_.empty())
+    {
+        return std::string("Missing required option '--font <file_path>'");
+    }
+    if (datafilepath_ != std::nullopt && datafilepath_->empty())
+    {
+        return std::string("Option '--data' requires a non-empty file path");
+    }
+
+    return std::nullopt;
+}
---
+++ src/CLI_args/CliArgs.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <string>
+#include <optional>
+#include <cstring>
+
+class CliArgs
+{
+public:
+    CliArgs(int argc, const char *argv[])
+        : argc_(argc - 1), argv_(argv + 1), program(argv[0])
+    {
+    }
+
+    std::string getProgramPath() const
+    {
+        return machine_code;
+    }
+    std::string getFontPath() const
+    {
+        return fontpath_;
+    }
+    std::optional<std::string> getDataFilePath() const
+    {
+        return (datafilepath_ != std::nullopt) ? std::make_optional(*datafilepath_) : std::nullopt;
+    }
+
+    void printUsage();
+
+    [[nodiscard]] std::optional<std::string> parse();
+
+private:
+    int argc_;
+    const char **argv_;
+    std::string program{};
+    std::string machine_code{};
+    std::string fontpath_{};
+    std::optional<std::string> datafilepath_{};
+};
