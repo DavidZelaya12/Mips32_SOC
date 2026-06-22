62f07a759eca05d8ede10c3968aaa2fa77320da0
DavidZelaya12 <davidzelaya06.isj@gmail.com>
2026-06-22 15:54:36 -0600

    Added CPU scaffold

---
+++ src/CPU/CPU.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include "Memory.h"
+#include "InstructionHandler.h"
+#include "RegisterFile.h"
+#include <iostream>
+
+class CPU
+{
+private:
+    Memory *memory;
+    RegisterFile *registerFile;
+    std::vector<uint32_t> instructionMemory;
+
+public:
+    CPU(std::vector<uint32_t> program)
+        : instructionMemory(program)
+    {
+        memory = new Memory();
+        registerFile = new RegisterFile();
+    };
+
+    ~CPU()
+    {
+        delete memory;
+        delete registerFile;
+    };
+
+    void Run();
+    void ExecuteInstruction();
+    void ExecRFormat(const Instruction::R_Format &rFormat);
+    void ExecIFormat(const Instruction::I_Format &iFormat);
+    void ExecJFormat(const Instruction::J_Format &jFormat);
+};
